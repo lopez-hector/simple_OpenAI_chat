e9e6acb0c555e0728180e95d07c541096654593b
hectorlopezhernandez <hlopezhernandez01@gmail.com>
2023-08-31 12:32:29 -0700

    small changes to handle quitting

--- chat.py
+++ chat.py
@@ -9,24 +9,52 @@ from langchain.prompts.chat import (
 
 import openai
 from pathlib import Path
+import argparse
 
-LLM = ChatOpenAI(model_name="gpt-4", max_tokens=2000)
+def main(model: str = 'gpt-3.5-turbo'):
 
-# what should the model do with your input
-conversation = []
-human_input = ''
-while human_input != 'quit':
-    # convert to format for model
+    LLM = ChatOpenAI(model_name=model, max_tokens=2000)
 
-    human_input = input('User: ')
-    human_message_prompt = HumanMessage(content=human_input)
-    conversation.append(human_message_prompt)
-    chat_prompt = ChatPromptTemplate.from_messages(conversation)
+    # what should the model do with your input
+    conversation = []
+    human_input = ''
+    while human_input != 'quit':
+        # convert to format for model
 
-    # run as a chain
-    chain = LLMChain(llm=LLM, prompt=chat_prompt, )
-    output = chain.run(input_language="English")
+        human_input = input('User: ')
 
-    system_message_prompt = SystemMessage(content=output)
-    conversation.append(system_message_prompt)
-    print(output)
+        if human_input.lower() == 'quit':
+            print('EXITING')
+            break
+
+        human_message_prompt = HumanMessage(content=human_input)
+        conversation.append(human_message_prompt)
+        chat_prompt = ChatPromptTemplate.from_messages(conversation)
+
+        # run as a chain
+        chain = LLMChain(llm=LLM, prompt=chat_prompt, )
+        output = chain.run(input_language="English")
+
+        system_message_prompt = SystemMessage(content=output)
+        conversation.append(system_message_prompt)
+        print(output)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Open AI chat")
+
+    # Add the model_name argument
+    parser.add_argument('-m',
+                        "--model_name",
+                        type=str,
+                        help="Name of the model")
+
+    args = parser.parse_args()
+
+    acceptable_models = set(['gpt-4', 'gpt-3.5-turbo'])
+
+    # Call the main function and pass the model_name argument
+    if args.model_name and args.model_name in acceptable_models:
+        main(model=model_name)
+    else:
+        print('defaulting to chat-gpt (gpt-3.5-turbo)')
+        main(model='gpt-3.5-turbo')
