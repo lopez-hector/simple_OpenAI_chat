1916704781522f5e796b3fae918ef14f0bb05836
hectorlopezhernandez <hlopezhernandez01@gmail.com>
2023-08-31 21:04:20 -0700

    updated chat logic

--- chat.py
+++ chat.py
@@ -4,6 +4,7 @@ from langchain.prompts.chat import (
     ChatPromptTemplate,
     SystemMessage,
     AIMessagePromptTemplate,
+    AIMessage,
     HumanMessage,
 )
 
@@ -11,17 +12,35 @@ import openai
 from pathlib import Path
 import argparse
 
-def main(model: str = 'gpt-3.5-turbo'):
 
+def main(model: str = 'gpt-3.5-turbo'):
     LLM = ChatOpenAI(model_name=model, max_tokens=2000)
 
+    system_message_prompt = SystemMessage(content="I'm a helpful assistant. If the user has no further questions or the conversation has clearly ended I will expcitly return the phrase: ```Have a nice day!!!``` without any added text.")
+
     # what should the model do with your input
-    conversation = []
+    conversation = [system_message_prompt]
     human_input = ''
     while human_input != 'quit':
         # convert to format for model
+        grab_input = []
+        while True:
+            if not grab_input:
+                grab_input.append(input('User: '))
+            else:
+                grab_input.append(input(''))
 
-        human_input = input('User: ')
+            if grab_input[-1] == '/end':
+                grab_input.pop()
+                break
+            elif grab_input[-1][-4:] == '/end':
+                grab_input[-1] = grab_input[-1][:-4]
+                break
+            elif grab_input[-1] == 'quit':
+                grab_input = ['quit']
+                break
+
+        human_input = ''.join(grab_input)
 
         if human_input.lower() == 'quit':
             print('EXITING')
@@ -35,9 +54,14 @@ def main(model: str = 'gpt-3.5-turbo'):
         chain = LLMChain(llm=LLM, prompt=chat_prompt, )
         output = chain.run(input_language="English")
 
-        system_message_prompt = SystemMessage(content=output)
-        conversation.append(system_message_prompt)
-        print(output)
+        if output.lower().strip() == 'Have a nice day!!!'.lower():
+            print('EXITING')
+            break
+
+        ai_message_prompt = AIMessage(content=output)
+        conversation.append(ai_message_prompt)
+        print('\n', output)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Open AI chat")
@@ -50,11 +74,11 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    acceptable_models = set(['gpt-4', 'gpt-3.5-turbo'])
+    acceptable_models = {'gpt-4', 'gpt-3.5-turbo'}
 
     # Call the main function and pass the model_name argument
     if args.model_name and args.model_name in acceptable_models:
-        main(model=model_name)
+        main(model=args.model_name)
     else:
         print('defaulting to chat-gpt (gpt-3.5-turbo)')
         main(model='gpt-3.5-turbo')
